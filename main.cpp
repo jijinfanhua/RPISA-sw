6e66b7926cd6348f72781ddb36fa967ee2d84f13
la pluma <lup19@mails.tsinghua.edu.cn>
2023-03-30 13:34:48 +0800

    add statistics function in main

--- main.cpp
+++ main.cpp
@@ -76,6 +76,14 @@ void testing_order(){
     }
 }
 
+float average(std::vector<int>& v){
+    float sum = 0;
+    for(auto item: v){
+        sum += item;
+    }
+    return sum / v.size();
+}
+
 int main(int argc, char** argv) {
     // phv[223]: packet id
     // phv[222]: tag
@@ -87,6 +95,8 @@ int main(int argc, char** argv) {
 
     int cycle = 0;
     int pkt = 0;
+    int output_pkt = 0;
+    std::vector<int> execute_latency;
     Switch switch_ = Switch();
     switch_.Config();
     for(int i = 0; i < 200000; i++) {
@@ -96,25 +106,27 @@ int main(int argc, char** argv) {
 //                switch_.Execute(0, Packet());
 //        }
 //        else{
-            string input = read_from_file(infile);
-            if(input == ""){
-                switch_.Execute(0, Packet(), cycle);
-            }
-            else{
-                pkt += 1;
-                switch_.Execute(1, input_to_packet(input), cycle);
-            }
+        string input = read_from_file(infile);
+        if(input == ""){
+            switch_.Execute(0, Packet(), cycle);
+        }
+        else{
+            pkt += 1;
+            switch_.Execute(1, input_to_packet(input), cycle);
+        }
 //        }
 
         auto output_arrive_id = switch_.get_output_arrive_id();
-            if(output_arrive_id.first != -1){
-                if(arrive_id_by_flow.find(output_arrive_id.first) == arrive_id_by_flow.end()){
-                    arrive_id_by_flow.insert({output_arrive_id.first, std::vector<int>({output_arrive_id.second})});
-                }
-                else{
-                    arrive_id_by_flow.at(output_arrive_id.first).push_back(output_arrive_id.second);
-                }
+        if(output_arrive_id.first != -1){
+            output_pkt += 1;
+            execute_latency.push_back(cycle - output_arrive_id.second);
+            if(arrive_id_by_flow.find(output_arrive_id.first) == arrive_id_by_flow.end()){
+                arrive_id_by_flow.insert({output_arrive_id.first, std::vector<int>({output_arrive_id.second})});
             }
+            else{
+                arrive_id_by_flow.at(output_arrive_id.first).push_back(output_arrive_id.second);
+            }
+        }
         cycle += 1;
     }
 
@@ -124,6 +136,8 @@ int main(int argc, char** argv) {
     cout << "total flow: " << arrive_id_by_flow.size() << endl;
     cout << "unordered flow: " << unordered_flow_count << endl;
 
-    cout << pkt << endl;
+    cout << "output pkt: " << output_pkt << endl;
+    cout << "input pkt: " << pkt << endl;
+    cout << "average execute latency: " << average(execute_latency) << endl;
     return 0;
 }
